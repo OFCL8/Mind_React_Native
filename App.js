175879063aa30dfad2c480c662d5c24e9bc9e16b
OFCL8 <oliver.carrillo@tectijuana.edu.mx>
2019-10-17 22:06:35 -0700

    Created Login page

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from 'expo-asset';
+import { AppLoading } from 'expo';
+import MusicApp from './app/index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+//Functions to load assets
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
+export default class App extends React.Component {
+
+  constructor() {
+    super()
+    //If app is ready to show image
+    this.state = {
+      isReady: false
+    }
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./assets/bg.jpg'),
+    ]);
+
+    await Promise.all([...imageAssets]);
+  }
+
+  render() {
+    //Checks if the state is not ready
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return <MusicApp />;
+  }
+
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,3 +56,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
