1e251d2c8d85bb99ac248c51dc05c49b3a6d49bf
OFCL8 <oliver.carrillo@tectijuana.edu.mx>
2019-10-23 10:50:47 -0700

    Created a default account to use firebase in the project and changed it's setup

--- components/Firebase.js
+++ components/Firebase.js
@@ -1,14 +1,15 @@
 import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
+  // Your web app's Firebase configuration
   const firebaseConfig = {
-  apiKey: "AIzaSyAwPfrJaJTW_Q5ipzJKmE9wxyzoojAG0RE",
-  authDomain: "mindreactnative.firebaseapp.com",
-  databaseURL: "https://mindreactnative.firebaseio.com",
-  projectId: "mindreactnative",
-  storageBucket: "mindreactnative.appspot.com",
-  messagingSenderId: "156896962944",
-  appId: "1:156896962944:web:0283c154297ac766fab965"
+    apiKey: "AIzaSyAILjr3lHU1uDpN0u_-g4dAN_h6FCNgB9A",
+    authDomain: "mindreactnative-76991.firebaseapp.com",
+    databaseURL: "https://mindreactnative-76991.firebaseio.com",
+    projectId: "mindreactnative-76991",
+    storageBucket: "mindreactnative-76991.appspot.com",
+    messagingSenderId: "354780949033",
+    appId: "1:354780949033:web:c49345ca0859a332df0a38"
 };
 export default class Firebase {
  
