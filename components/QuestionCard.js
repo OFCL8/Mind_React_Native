5f73170d37a744d066fee822614e1c2d5133f46e
RobStein96 <49456466+RobStein96@users.noreply.github.com>
2019-11-25 11:47:04 -0800

    Survey answer selection

--- components/QuestionCard.js
+++ components/QuestionCard.js
@@ -5,20 +5,56 @@ var numbers = ['1','2','3','4','5'];
 
 class QuestionCard extends React.Component {
 
+  state = {
+    selected: [false,false,false,false,false],
+    select: false,
+    refresh: false,
+    prevSelect: 0,
+  }
+
   renderButton = ({index,item}) => {
   
     const answerQuestion = () => {
       this.props.onPress(index+1);
+/*
+      if(!this.state.select) {
+        this.state.prevSelect = index;
+        this.state.select = true;
+      }
+      else {
+        this.state.selected[this.state.prevSelect] = false;  
+        this.state.select = false;
+        this.state.prevSelect = index;
+      }
+      this.state.selected[index] = !this.state.selected[index];
+ */
+      
+
+      if(this.state.select === true) {
+        this.state.selected[this.state.prevSelect] = false;
+      }
+      
+      this.state.selected[index] = !this.state.selected[index];
+      this.state.select = true;
+      this.state.prevSelect = index;
+
+      this.setState({
+        selected: this.state.selected, 
+        select: this.state.select,
+        refresh: !this.state.refresh,
+        prevSelect: this.state.prevSelect,
+      });
     }
 
     return(
-      <TouchableHighlight style = {styles.button} onPress = {answerQuestion} >
+      <TouchableHighlight style = {this.state.selected[index] ? styles.buttonSelected : styles.button} onPress = {answerQuestion} >
         <Text style = {styles.buttonText}>{index+1}</Text>
       </TouchableHighlight>
     );
   }
 
   render() {
+    console.log('\n\nselected: ' + this.state.selected + '\nselect: ' + this.state.select + '\nprevSelect: ' + this.state.prevSelect);
     return (
       <View style = {styles.container}>
         <Text style = {styles.text}>{this.props.question}</Text>
@@ -32,6 +68,7 @@ class QuestionCard extends React.Component {
         >
           <FlatList
             data = {numbers}
+            extraData = {this.state.refresh}
             renderItem = {this.renderButton}
             numColumns = {5}
             keyExtractor = {(item,index) => index.toString()}
@@ -52,6 +89,12 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: 'lightgrey',
+    borderRadius: 10,
+    width: 50,
+    margin: 2
+  },
+  buttonSelected: {
+    backgroundColor: 'red',
     borderRadius: 10,
     width: 50,
     margin: 2
